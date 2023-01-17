4c303449f532051356bd67fdb69e5c0308b6eda2
josefaidt <josef.aidt@gmail.com>
2023-01-17 10:58:50 -0800

    🚀

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,17 @@
+import { useEffect } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
+import { useSession } from 'next-auth/react'
 import styles from '@/styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const { data: session } = useSession()
+  useEffect(() => {
+    console.log('session is', session)
+  }, [session])
   return (
     <>
       <Head>
